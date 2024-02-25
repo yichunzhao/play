9156ddf069c353c9c315dd8298e313743e716f3d
Yichun Zhao <zhaoyichun@hotmail.com>
2024-02-25 14:39:35 +0100

    refactoring

--- src/main/java/com/ynz/sandbox/play/parse/UsingErrorCodeActionMapper.java
+++ src/main/java/com/ynz/sandbox/play/parse/UsingErrorCodeActionMapper.java
@@ -5,10 +5,9 @@ public class UsingErrorCodeActionMapper {
     public static void main(String[] args) {
         var mapper = ErrorCodeActionMapper.getInstance();
 
-        System.out.println(mapper.toString());
+        System.out.println(mapper);
 
         var errorCodeMap = mapper.getErrorCodeMap("E001");
         errorCodeMap.ifPresent(System.out::println);
-
     }
 }
